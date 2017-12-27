e1992f09d967163301d5ce68e466260027c8d6e9
Muhammad Robi <muhammadrobi65@gmail.com>
2017-12-27 10:35:36 +0700

    1. edit eyemarket view 2. edit order_mail view for lunas condition
    --robi

--- application/views/eyemarket/new_view/template.php
+++ application/views/eyemarket/new_view/template.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,500,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Bootstrap and Font Awesome css -->
-    <link href="<?php echo base_url(); ?>bs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>bs/fa/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>bs/jud/css/bootstrap.min.css ">
 
     <!-- Css animations  -->
@@ -33,15 +33,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
     <!-- Favicon and apple touch icons-->
-    <link rel="shortcut icon" href="<?=base_url()?>bs/jud/img/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="<?=base_url()?>bs/jud/img/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?=base_url()?>bs/jud/img/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="<?=base_url()?>bs/jud/img/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url()?>bs/jud/img/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="<?=base_url()?>bs/jud/img/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="<?=base_url()?>bs/jud/img/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="<?=base_url()?>bs/jud/img/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="<?=base_url()?>bs/jud/img/apple-touch-icon-152x152.png" />
+    <link rel="shortcut icon" href="<?=base_url()?>img/tab_icon.png" type="image/x-icon" />
 </head>
 
 <body>
