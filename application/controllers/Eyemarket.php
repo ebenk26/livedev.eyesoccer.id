ad81e0e04607d6bf5a9a56541974690323a0a9b3
Muhammad Robi <muhammadrobi65@gmail.com>
2018-01-04 10:41:48 +0700

    1. edit eyemarket controller function order_fix 2. edit view mail_order 3. add tooltip about jne on order review
    --robi

--- application/controllers/Eyemarket.php
+++ application/controllers/Eyemarket.php
@@ -742,23 +742,24 @@ class Eyemarket extends CI_Controller {
 	{
 		$data['harga'] 		= $this->Eyemarket_model->get_total_harga($id_member);
 		$id_kurir 			= $_POST["delivery"];
+		$ongkir 			= $this->input->post('ongkir'.$id_kurir);
 
 		$data1 = array(
 			'id_kurir' 	=> $id_kurir,
+			'ongkir' 	=> $ongkir,
 		);
 
-		$ongkir 	= $this->input->post('ongkir'.$id_kurir);
-		$harga  	= $data['harga']->total_all;
-		$total_all 	= $ongkir + $harga;
+		// $harga  	= $data['harga']->total_all;
+		// $total_all 	= $ongkir + $harga;
 
-		$data2 = array(
-			'id_kurir' 		=> $id_kurir,
-			'ongkir' 		=> $ongkir,
-			'harga_all' 	=> $total_all,
-		);
+		// $data2 = array(
+		// 	'id_kurir' 		=> $id_kurir,
+		// 	'ongkir' 		=> $ongkir,
+		// 	'harga_all' 	=> $total_all,
+		// );
 
 		$update_cart 	= $this->Eyemarket_model->update_cart_delivery($id_member,$data1);
-		$update_order 	= $this->Eyemarket_model->update_order_delivery($id_member,$data2);
+		// $update_order 	= $this->Eyemarket_model->update_order_delivery($id_member,$data2);
 
 		
 		redirect('/eyemarket/order_payment/'.$id_member);
@@ -797,10 +798,14 @@ class Eyemarket extends CI_Controller {
 			$id_alamat 		= $model['id_alamat'];
 			$id_kurir 		= $model['id_kurir'];
 			$id_tipe_bayar 	= $model['id_tipe_bayar'];
+			$ongkir 		= $model['ongkir'];
 		}
 
 		$data['total_all']	= $this->Eyemarket_model->get_total_harga($id_member);
 		$data['berat_all']	= $this->Eyemarket_model->get_total_berat($id_member);
+
+		$harga  	= $data['total_all']->total_all;
+		$total_all 	= $ongkir + $harga;
 
 		// ====== input ke table order 
 		$data 	= array(
@@ -808,8 +813,10 @@ class Eyemarket extends CI_Controller {
 			'id_alamat' 	=> $id_alamat,
 			'id_kurir' 		=> $id_kurir,
 			'id_tipe_bayar' => $id_tipe_bayar,
-			'harga' 		=> $data['total_all']->total_all,
+			'harga' 		=> $harga,
+			'ongkir' 		=> $ongkir,
 			'berat_all' 	=> $data['berat_all']->berat_all,
+			'harga_all' 	=> $total_all,
 		);
 
 		$order 		= $this->Eyemarket_model->set_order($data);
@@ -896,7 +903,7 @@ class Eyemarket extends CI_Controller {
 		$data['no_invoice'] 	= $no_invoice;
 
 		//=====set expired time (4 jam)
-		$expired 	= date('Y-m-d H:i:s',strtotime("+4 hours"));
+		$data['expired'] 	= date('Y-m-d H:i:s',strtotime("+4 hours"));
 
 		//=====set status = 1 di keranjang
 		$cart 		= array(
@@ -911,15 +918,14 @@ class Eyemarket extends CI_Controller {
 			'no_order'	 	=> $no_order,
 			'no_invoice'	=> $no_invoice,
 			'order_date' 	=> date("Y-m-d H:i:s"),
-			'expired_date' 	=> $expired,
+			'expired_date' 	=> $data['expired'],
 			'status' 		=> 1,
 		);
 
-		$update_order 	=  $this->Eyemarket_model->set_order_status($id_order,$object);
-
+		$update_order 		= $this->Eyemarket_model->set_order_status($id_order,$object);
+		
 		if ($update_order)
 		{
-
 			$message=$this->load->view('eyemarket/new_view/mail_order',$data,TRUE);
 
 			$objMail 	= $this->phpmailer_library->load();
@@ -957,7 +963,7 @@ class Eyemarket extends CI_Controller {
 				if ($objMail->send())
 				{
 					// echo "bisa";
-					$this->session->set_flashdata('sukses','Order anda berhasil, silahkan cek email anda untuk langkah selanjutnya');
+					$this->session->set_flashdata('sukses','<strong>Order anda berhasil</strong>, silahkan cek email anda untuk langkah selanjutnya');
 
 					redirect('/eyemarket/pesanan/'.$id_member);
 				}
@@ -975,20 +981,16 @@ class Eyemarket extends CI_Controller {
 
 	public function invoice($no_order)
 	{
-		$this->mod->checkLogin();
 		
-		if ($this->id_member != $id_member)
-		{
-			redirect('/');
-		}
-		else
-		{
-			$data['model'] 		= $this->Eyemarket_model->get_invoice($no_order);
+		$url 	= current_url();
 
-			$data['cart'] 		= $this->Eyemarket_model->get_keranjang_invoice($data['model']->id);
+		$this->mod->checkLogin($url);
 
-			$this->load->view('/eyemarket/new_view/invoice', $data);
-		}
+		$data['model'] 		= $this->Eyemarket_model->get_invoice($no_order);
+
+		$data['cart'] 		= $this->Eyemarket_model->get_keranjang_invoice($data['model']->id);
+
+		$this->load->view('/eyemarket/new_view/invoice', $data);
 	}
 
 	public function user($id_member)
