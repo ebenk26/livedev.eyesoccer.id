055becac0267704543c1fa9adcb792323c381f7d
rizki <rizki@intelix.co.id>
2018-01-05 10:24:10 +0700

    update fb comment detail Rizki

--- application/views/eyenews/detail.php
+++ application/views/eyenews/detail.php
@@ -227,7 +227,7 @@
                     <div style="margin-top: 20px;">
                         <span style="font-size: 17px;font-weight: 600;color: rgb(41, 41, 41);">Komentar</span>
                     </div>
-					<div class="fb-comments" data-href="http://eyesoccer.id<?=$_SERVER['REQUEST_URI']?>" data-numposts="5"></div>					
+					<div class="fb-comments" data-href="<?=base_url();?><?=$_SERVER['REQUEST_URI']?>" data-numposts="5"></div>					
                 </div>
 
                 <div class="container tube-r">
