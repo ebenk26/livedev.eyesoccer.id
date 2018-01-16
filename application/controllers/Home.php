d9b73149e7babba112c67e094bbba7b0bb9b7ce3
rizki <rizki@intelix.co.id>
2018-01-16 17:10:51 +0700

    update home controller Rizki

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -386,6 +386,7 @@ class Home extends CI_Controller {
 			$pic="foto-"."profile-".rand("1000","9999")."-".$_FILES['pic']['name'];
 			$pic = preg_replace('/\s+/', '', $pic);
 			move_uploaded_file($_FILES['pic']['tmp_name'], pathUrl()."assets/img_storage/".$pic);
+			var_dump(pathUrl());exit();
 				$last_id = $_SESSION["member_id"];
 			$post_data = array(
 				'title'			=> $caption,
