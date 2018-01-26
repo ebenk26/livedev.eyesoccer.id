dc5246f20a869bc4624bdbac2cc44789433ee4d9
unuha26 <unuha26@users.noreply.github.com>
2018-01-27 05:16:56 +0700

    eprofile hampir jadi

--- application/views/eyeprofile/pemain_detail.php
+++ application/views/eyeprofile/pemain_detail.php
@@ -31,24 +31,26 @@ $bulan 	= array(
 '11' => 'November',
 '12' => 'Desember',
 );
-?>
-<br><br><br><br>
+?></div>
+<div class="w-blue">
+    <img src="http://localhost/beta.eyesoccer.id/assets/img/segitiga-putih-01.png" alt="">
+</div><div class="desktop">
     <div class="container">
         <div class="garis-banner over-in profile-pemain">
             <div class="left">
-                <svg style="height: 189px;">
+                <!-- <svg style="height: 189px;">
                     <g id="Layer_2" data-name="Layer 2">
                         <g id="Layer_1-2" data-name="Layer 1">
                             <polygon class="fill" points="132 0 22 190 0 190 110 0 132 0" />
                             <polygon class="fill" points="330 0 330 190 42 190 152 0 330 0" />
                         </g>
                     </g>
-                </svg>
+                </svg> -->
                 <div class="box-img-radius">
                     <img src="<?=imgUrl()?>systems/player_storage/<?=$pemain["pic"]?>" alt="">                        
                 </div>
             </div>
-            <div class="right fill">
+            <div class="right">
                 <div class="t-30 mt-53">
                     <table>
                         <tbody>
@@ -108,8 +110,8 @@ $bulan 	= array(
         </div>
     </div>
 
-    <div class="desktop pd-t-280">
-    <div class="center-desktop m-0">
+    <div class="desktop">
+    <div class="center-desktop m-0 pd-t-100" >
         <div class="w-60 m-r-1 pd-t-20 formasi">
             <div class="container">
                 <h3>Karir Klub</h3>
