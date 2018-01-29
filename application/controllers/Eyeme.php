c30cfd43bfe355c3e171ce6e02f1e62ed55a92a8
sofyanwaldy <sofyanwaldi96@gmail.com>
2018-01-29 11:07:47 +0700

    pesan error

--- application/controllers/Eyeme.php
+++ application/controllers/Eyeme.php
@@ -357,7 +357,7 @@ class Eyeme extends CI_Controller {
 						'active'      => '1');
 
 		$this->db->insert('me_img',$dataImg);
-		$arr      = array('msg'=> 'Berhasil Upload Gambar');
+		$arr['msg']  = (!$do ? 'Berhasil Upload Foto' : 'gagal upload Foto');
 		$response = json_encode($arr);
 		echo '<script>alert(\''.$arr['msg'].'\')</script>';
 		redirect(MEPROFILE.$this->username,'refresh');
