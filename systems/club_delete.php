0c6a0e9455ce071f11d86ef8b90f19c8b34522ac
Kharizuno <kharizuno@yahoo.com>
2018-02-23 10:59:15 +0700

    beta web

--- systems/club_delete.php
+++ systems/club_delete.php
@@ -3,20 +3,20 @@
 require "check_login.php";
 require "insert_logs.php";
 
-$admin_id=$_GET['admin_id'];
+$admin_id = $_GET['admin_id'];
 
-$club_id=$_GET['club_id'];
+$club_id = $_GET['club_id'];
 
-$ins = insertLog('tbl_club','club_id',$club_id,'delete',$admin_id,$con,$ip);
-if($ins=="sukses"){
-	// echo $ins;exit();
-	$cmd=mysqli_query($con,"delete from tbl_club where club_id='$club_id'");
+$ins = insertLog('tbl_club', 'club_id', $club_id, 'delete', $admin_id, $con, $ip);
+if ($ins == "sukses") {
+    // echo $ins;exit();
+    $cmd = mysqli_query($con, "delete from tbl_club where club_id='$club_id'");
 
-	$cmd1=mysqli_query($con,"delete from tbl_player where club_id='$club_id'");
+    $cmd1 = mysqli_query($con, "delete from tbl_player where club_id='$club_id'");
 
-	$cmd1=mysqli_query($con,"delete from tbl_official_team where club_id='$club_id'");
-}else{
-	// echo $ins;exit();
+    $cmd1 = mysqli_query($con, "delete from tbl_official_team where club_id='$club_id'");
+} else {
+    // echo $ins;exit();
 }
 // $cmd=mysqli_query($con,"delete from tbl_club where club_id='$club_id'");
 
@@ -26,6 +26,6 @@ if($ins=="sukses"){
 
 header("location:club?admin_id=$admin_id");
 
-$_SESSION['admin_id']; 
+$_SESSION['admin_id'];
 
 ?>
