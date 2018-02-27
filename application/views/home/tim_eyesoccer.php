0ef481006590b6117ed28ad83f5afe40f51f7e01
faradiland <fndanopa@gmail.com>
2018-02-27 17:34:25 +0700

    add

--- application/views/home/tim_eyesoccer.php
+++ application/views/home/tim_eyesoccer.php
@@ -15,10 +15,10 @@
                     <table class="mng mb-20">
                         <tr>
                             <td>
-                                <img src="assets/img/EYEME/user-discover.png" alt="">
+                                <img src="assets/img/cheppy.jpg" alt="">
                             </td>
                             <td>
-                                <img src="assets/img/EYEME/user-discover.png" alt="">
+                                <img src="assets/img/tomkur.jpg" alt="">
                             </td>
                             <td>
                                 <img src="assets/img/EYEME/user-discover.png" alt="">
@@ -58,6 +58,7 @@
                             <span>
                                 <ey>Reporter dan Jurnalis</ey>
                             </span>
+                            <span>Ari Dwi Prasetyo</span>
                             <span>Bayu Mahendra</span>
                             <span>Aditya Gilang</span>
                             <span>Ridhoni</span>
@@ -72,12 +73,15 @@
                         </div>
                         <div class="container w265 tim">
                             <span>
-                                <ey>Head of EyeTube Division</ey>
+                                <ey>EyeTube Division</ey>
                             </span>
                             <span>Fadia Sekar</span>
                             <span>Ade Firmansyah</span>
                             <span>I Gede Agus Sukra Tanaya</span>
                             <span>Ondri S. Prakarsa</span>
+                            <span>Pratama Ilmiawan</span> <br/>
+                            <span><ey>Eyemarket Division</ey></span>
+                            <span>Soraya Noor</span>
                         </div>
                         <div class="container w265 tim">
                             <span>
@@ -129,7 +133,8 @@
                         <span>
                             <ey>Internal Relation</ey>
                         </span>
-                        <span>Noor Aliyah</span>
+                        <span>Siska Oktavia Lestari</span>
+                        <span>Abdul Aziz</span>
                         <br>
                         <br>
                     </div>
@@ -137,7 +142,8 @@
                         <span>
                             <ey>Graphic Design</ey>
                         </span>
-                        <span>Fauzan</span>
+                        <span>Nur Leili Irma Bayinah</span>
+                        <span>Emil Siregar</span>
                         <br>
                         <br>
                     </div>
@@ -160,7 +166,7 @@
                             <ey>Social Media Specialist</ey>
                         </span>
                         <span>Nugraha Putra</span>
-                        <span>Revita</span>
+                        <span>Revita Putri Elistine</span>
                         <br>
                     </div>
                     <div class="container w355 tim">
@@ -185,28 +191,39 @@
                     <div class="dline m-0 m-t-10 mb-30"></div>
                     <div class="container w355 tim">
                         <span>
-                            <ey>Back End</ey>
+                            <ey>Back End Developer</ey>
                         </span>
                         <span>Muhammad Robi</span>
                         <span>Rizki Satrio Putro</span>
-                        <span>Yudi Santoso</span>
+                        <span>Kharizuno Hizbullah</span>
+                        <span>Sopyan Waldy</span>
                         <br>
                         <br>
                     </div>
                     <div class="container w355 tim">
                         <span>
-                            <ey>Front End</ey>
+                            <ey>Front End Developer</ey>
                         </span>
                         <span>Fajar Ulin Nuha</span>
                         <span>Faradilah Nurzahra Danopa</span>
                         <br>
+                        <span>
+                            <ey>SEO Specialist</ey>
+                        </span>
+                        <span>Ardiatma</span>
                         <br>
                     </div>
                     <div class="container w355 tim">
                         <span>
-                            <ey>SEO Specialist</ey>
+                            <ey>Mobile App Developer</ey>
                         </span>
-                        <span>Andri Sukma Varoga</span>
+                        <span>Fazil Muamar</span>
+                        <span>Naufal Syarifuddin</span>
+                        <br>
+                        <span>
+                            <ey>IT Infrastructure</ey>
+                        </span>
+                        <span>Jimmy Zakaria</span>
                     </div>
                 </div>
             </div>
