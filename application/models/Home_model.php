c8651e7e95829773f936fec7814a06f91f122d54
rizki <rizki@intelix.co.id>
2018-03-06 11:59:17 +0700

    update rekomen 2

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -477,6 +477,8 @@ class Home_model extends CI_Model
 										tbl_eyenews a
 									WHERE
 										a.publish_on<='".date("Y-m-d H:i:s")."'
+									AND
+										a.category_news = 2
 									ORDER BY
 										a.eyenews_id DESC
 									LIMIT
