24ad6d9c967b4872fce3b7937fbac3b3d3ee406e
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-03-16 15:03:50 +0700

    test eyeme mac

--- application/views/eyeme/footer.php
+++ application/views/eyeme/footer.php
@@ -180,13 +180,18 @@ var valCom = $(this).val();
 });
 
 //upload click event
-$('#upload,.upl').click(function(e) {
-    /* Act on the event */
+$(document).on('click','#upload,.upl',function(e){
     e.preventDefault();
-     $('#upload_pop').css('display','block');
+    $('#upload_pop').css('display','block');
+
+})
+// $('#upload,.upl').click(function(e) {
+//     /* Act on the event */
+//     e.preventDefault();
+//      $('#upload_pop').css('display','block');
 
 
-});
+// });
 
 //esc press event
 $(document).keyup(function(e) {  
