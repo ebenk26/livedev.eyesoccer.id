263513e7d2794252fa5198ec2d40db3d15135851
rizki <rizki@intelix.co.id>
2018-04-05 11:26:45 +0700

    update tag manager

--- application/views/template/static.php
+++ application/views/template/static.php
@@ -147,7 +147,27 @@
 █████████████████████████████████████████████
 ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
  -->
-			
+		<?php
+			$blacklist = array(
+				'127.0.0.1',
+				'localhost',
+				'::1'
+			);
+
+			if(!in_array($_SERVER['REMOTE_ADDR'], $blacklist)){
+		?>
+				<!-- Google Tag Manager -->
+				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-P7HQLCC');</script>
+				<!-- End Google Tag Manager -->
+		<?php
+			}else{
+				echo "ssss";
+			}
+		?>
     </head>
 
     <?php
@@ -468,5 +488,23 @@
             </td>
         </tr>
     </table>
+		<?php
+			$blacklist = array(
+				'127.0.0.1',
+				'localhost',
+				'::1'
+			);
+
+			if(!in_array($_SERVER['REMOTE_ADDR'], $blacklist)){
+		?>
+				<!-- Google Tag Manager (noscript) -->
+				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P7HQLCC"
+				height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+				<!-- End Google Tag Manager (noscript) -->
+		<?php
+			}else{
+				echo "ssss";
+			}
+		?>
     </body>
 </html>
