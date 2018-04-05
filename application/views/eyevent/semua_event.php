0d5298db36be9799dc07550e6bb59793860deafe
Muhammad Robi <muhammadrobi65@gmail.com>
2018-04-05 14:19:54 +0700

    ubah link eyevent detail
    --robi

--- application/views/eyevent/semua_event.php
+++ application/views/eyevent/semua_event.php
@@ -13,7 +13,7 @@
             {
         ?>
                 <div class="w4">
-                    <a href="<?= base_url(); ?>eyevent/detail/<?= $value->id_event; ?>">
+                    <a href="<?= base_url(); ?>eyevent/detail/<?= $value->slug; ?>">
                         <div class="w4-f p-r">
                             <img src="<?= $value->url_pic; ?>" style="min-width:100%;height: 100%;position: absolute;top: 50%;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);">
                         </div>
