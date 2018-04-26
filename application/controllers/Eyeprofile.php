4a447af24e337fc06a5391bfc76dadaaf9ceb2a2
sofyanwaldi96@gmail.com <sofyanwaldi96@gmail.com>
2018-04-26 11:00:35 +0700

    penyesuaian live

--- application/controllers/Eyeprofile.php
+++ application/controllers/Eyeprofile.php
@@ -186,8 +186,8 @@ class Eyeprofile extends CI_Controller {
 	
 	public function official()
 	{
-		
-		$data['get_all_kompetisi'] = $this->Eyeprofile_model->get_all_kompetisi();		
+		$data['get_all_liga'] = $this->Eyeprofile_model->get_all_liga();
+		$data['competition'] = $this->Eyeprofile_model->get_all_kompetisi();		
 		$data['kanal'] = "home";
 		
 		$data['kanal'] = "home";
